55dbb36ad833180deb3a087e26d8b999b30b7fb2
ssln5014 <81206855+ssln5014@users.noreply.github.com>
2024-10-19 23:58:44 +0800

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -31,14 +31,14 @@ export function formatBytes(bytes: number, decimals: number = 2) {
   const dm = decimals < 0 ? 0 : decimals;
   const sizes = [
     "Bytes",
-    "KiB",
-    "MiB",
-    "GiB",
-    "TiB",
-    "PiB",
-    "EiB",
-    "ZiB",
-    "YiB",
+    "KB",
+    "MB",
+    "GB",
+    "TB",
+    "PB",
+    "EB",
+    "ZB",
+    "YB",
   ];
 
   const i = Math.floor(Math.log(bytes) / Math.log(k));
